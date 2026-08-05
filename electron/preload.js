cb573d13ed1b710b4bae19feada4551d26534a17
Akhand6886 <aksingh6886@gmail.com>
2026-08-05 17:30:15 +0530

    chore: remove Electron infrastructure and Next.js configuration files

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getVersion: () => ipcRenderer.invoke('app:get-version'),
-  getPlatform: () => ipcRenderer.invoke('app:get-platform'),
-  isDesktop: true
-});
